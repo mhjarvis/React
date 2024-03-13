07c06daaf9d15c851b135a8de54b9fe2aedb7072
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-13 18:52:45 -0500

    Add images via import; using javascript variables in jsx

--- Tutorials/01-starting-project/src/App.jsx
+++ Tutorials/01-starting-project/src/App.jsx
@@ -1,19 +1,42 @@
+// This imports a image file into the javascript file - a better approach to getting images in
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+    // This is a dynamic variable used with the getRandomInt function
+    // Remove variables from jsx code for a leaner approach
+    const description = reactDescriptions[genRandomInt(3)];
+
+    return (
+        <header>
+            {/* 
+                `reactImg` is brought into the application after importing it
+                It is a variable that acts as a pointer to the path where the image is stored
+            */}
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {description} React concepts you will need for almost any app
+                you are going to build!
+            </p>
+        </header>
+    );
+}
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
