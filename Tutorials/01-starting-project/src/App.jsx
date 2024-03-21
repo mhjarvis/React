b81a8d90f34c72aa706122c600e8c6070e022fa5
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-21 12:20:49 -0500

    Add CoreConcept component

--- Tutorials/01-starting-project/src/App.jsx
+++ Tutorials/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 // This imports a image file into the javascript file - a better approach to getting images in
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -28,11 +29,34 @@ function Header() {
     );
 }
 
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt="..." />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header />
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The core UI building block."
+                            image={componentsImg}
+                        />
+                        <CoreConcept />
+                        <CoreConcept />
+                        <CoreConcept />
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
