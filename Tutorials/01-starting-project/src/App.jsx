676db998f4fc68c41b292493738746a9ded817e1
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-26 18:58:57 -0500

    Add components folder and move Header into seperate component

--- Tutorials/01-starting-project/src/App.jsx
+++ Tutorials/01-starting-project/src/App.jsx
@@ -1,33 +1,7 @@
 // This imports a image file into the javascript file - a better approach to getting images in
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
+import Header from './components/Header.jsx'
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-    // This is a dynamic variable used with the getRandomInt function
-    // Remove variables from jsx code for a leaner approach
-    const description = reactDescriptions[genRandomInt(3)];
-
-    return (
-        <header>
-            {/* 
-                `reactImg` is brought into the application after importing it
-                It is a variable that acts as a pointer to the path where the image is stored
-            */}
-            <img src={reactImg} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {description} React concepts you will need for almost any app
-                you are going to build!
-            </p>
-        </header>
-    );
-}
 
 // This example uses OBJECT DESTRUCTURING for importing object variables
 function CoreConcept({ image, title, description }) {
---
+++ Tutorials/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import reactImg from "../assets/react-core-concepts.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+    // This is a dynamic variable used with the getRandomInt function
+    // Remove variables from jsx code for a leaner approach
+    const description = reactDescriptions[genRandomInt(3)];
+
+    return (
+        <header>
+            {/* 
+                `reactImg` is brought into the application after importing it
+                It is a variable that acts as a pointer to the path where the image is stored
+            */}
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {description} React concepts you will need for almost any app
+                you are going to build!
+            </p>
+        </header>
+    );
+}
