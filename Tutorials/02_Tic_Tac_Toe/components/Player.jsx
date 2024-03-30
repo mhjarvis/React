36c2be07f25dc66a0e9c0a12ef81fd058686fb9f
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-30 17:31:33 -0500

    Update Player Component

---
+++ Tutorials/02_Tic_Tac_Toe/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((lastValue) => !lastValue);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    let btnCaption = "Edit";
+
+    if (isEditing) {
+        playerName = <input type="text" required value={name}></input>;
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {playerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>
+                {isEditing ? "Save" : "Edit"}
+            </button>
+        </li>
+    );
+}
