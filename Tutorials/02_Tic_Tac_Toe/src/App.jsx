24c8bdc9127f91f6742fb7e39a22ad015740f2d9
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-01 10:19:07 -0500

    Add GameBoard component and import into App.jsx

--- Tutorials/02_Tic_Tac_Toe/src/App.jsx
+++ Tutorials/02_Tic_Tac_Toe/src/App.jsx
@@ -1,13 +1,15 @@
 import Player from "../components/Player";
+import GameBoard from "../components/GameBoard";
 
 function App() {
     return (
         <main>
             <div id="game-container">
                 <ol id="players">
-                    <Player initialName='player 1' symbol='X' />
-                    <Player initialName='player 2' symbol='O' />
+                    <Player initialName="player 1" symbol="X" />
+                    <Player initialName="player 2" symbol="O" />
                 </ol>
+                <GameBoard />
             </div>
         </main>
     );
