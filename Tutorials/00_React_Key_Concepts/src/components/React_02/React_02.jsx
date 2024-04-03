b8f517e5a73771db1196a2762b66a98ea3ba4ee7
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-02 22:25:38 -0500

    Update questions for section 2

--- Tutorials/00_React_Key_Concepts/src/components/React_02/React_02.jsx
+++ Tutorials/00_React_Key_Concepts/src/components/React_02/React_02.jsx
@@ -1,10 +1,13 @@
 import Title from "../Title";
 import "./React_02.css";
+import { questions_02 } from "./react_02_questions";
+import TestYourKnowledge from "../TestYourKnowledge";
 
 export default function React_02() {
     return (
         <div>
             <Title>2. Understanding React Components and JSX</Title>
+            <TestYourKnowledge questions={questions_02} />
         </div>
     );
 }
