b140bf7e3480cadd9d2fe6926e1aca57a2b4fe9f
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-03 17:30:08 -0500

    Add Activity 5.1 solution - creating error message using conditionals and state variable

--- Tutorials/00_React_Key_Concepts/src/components/React_05/Activity_5_1/Form.jsx
+++ Tutorials/00_React_Key_Concepts/src/components/React_05/Activity_5_1/Form.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 export default function Form() {
+    const [email, setEmail] = useState("");
+    const [showError, setShowError] = useState(false);
+
+    function emailInputHandler(event) {
+        setEmail(event.target.value);
+    }
+
+    function emailAddressHandler() {
+        if (email.includes("@")) {
+            setShowError(false);
+        } else {
+            setShowError(true);
+        }
+    }
+
     return (
         <div className="form_5_1">
             <label htmlFor="email">Your Email</label>
-            <input type="text" placeholder="Enter email" />
-            <button>Submit</button>
+            <input
+                type="text"
+                placeholder="Enter email"
+                onChange={emailInputHandler}
+                value={email}
+            />
+            <p className="error-message">
+                {showError ? "Invalid email address entered" : ""}
+            </p>
+            <button onClick={emailAddressHandler}>Submit</button>
         </div>
     );
 }
