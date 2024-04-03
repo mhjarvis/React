7dbc0964fba8490fa8d657e076521d03fcccb92e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-02 22:32:06 -0500

    Add Activity 2.1 css and import as needed

--- Tutorials/00_React_Key_Concepts/src/components/React_02/React_02.jsx
+++ Tutorials/00_React_Key_Concepts/src/components/React_02/React_02.jsx
@@ -2,12 +2,14 @@ import Title from "../Title";
 import "./React_02.css";
 import { questions_02 } from "./react_02_questions";
 import TestYourKnowledge from "../TestYourKnowledge";
+import Activity_2_1 from "./Activity_2_1";
 
 export default function React_02() {
     return (
         <div>
             <Title>2. Understanding React Components and JSX</Title>
             <TestYourKnowledge questions={questions_02} />
+            <Activity_2_1 />
         </div>
     );
 }
