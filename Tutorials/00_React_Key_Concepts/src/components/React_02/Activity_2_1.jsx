89b3050aa3832ae8181ed20ce4899c516c8b99b5
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-02 22:31:52 -0500

    Add activity 2.1

---
+++ Tutorials/00_React_Key_Concepts/src/components/React_02/Activity_2_1.jsx
@@ -0,0 +1,9 @@
+export default function Activity_2_1() {
+    return (
+        <div className="activity_2_1">
+            <h2>Activity 2.1: Creating a React App to Present Yourself</h2>
+            <h3>Hi, this is Markus!</h3>
+            <p>More information about me that I dont want to share...</p>
+        </div>
+    );
+}
