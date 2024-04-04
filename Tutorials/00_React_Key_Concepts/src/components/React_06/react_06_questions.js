e7c23c523024a326e18bbe5477ed1bea3b98d0c5
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-03 20:37:10 -0500

    Add questions for section 6

--- Tutorials/00_React_Key_Concepts/src/components/React_06/react_06_questions.js
+++ Tutorials/00_React_Key_Concepts/src/components/React_06/react_06_questions.js
@@ -1,15 +1,15 @@
 const questions_06 = [
+    ["1. With which language are styles for React components defined?", ""],
     [
-        "1. What is 'conditional content'?",
-        "Content that should only be displayed under certain circumstances.",
+        "2. Which important difference between projects with and without React has to be kept in mind when assigning classes to elements?",
+        "",
     ],
-    ["", ""],
-    ["", ""],
-    ["", ""],
-    ["", ""],
-    ["", ""],
-    ["", ""],
-    ["", ""],
+    ["3. How can styles be assigned dynamically and conditionally?", ""],
+    ["4. What does 'scoping' mean in the context of styling?", ""],
+    [
+        "5. How could styles be scoped to components? Briefly explain at least one concept that helps with scoping.",
+        "",
+    ],
 ];
 
 export { questions_06 };
