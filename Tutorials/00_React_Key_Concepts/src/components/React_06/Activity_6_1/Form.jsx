f98e2890be459c5e3df8d2d59831b0ebe840ba5e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-05 20:59:29 -0500

    Add state variables and update functions

--- Tutorials/00_React_Key_Concepts/src/components/React_06/Activity_6_1/Form.jsx
+++ Tutorials/00_React_Key_Concepts/src/components/React_06/Activity_6_1/Form.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 export default function Form() {
-    return <div>
-        <form action="">
-            <label htmlFor="email">Your email</label>
-            <input type="email" id="email" />
-            <label htmlFor="password">Your password</label>
-            <input type="text" id='password' />
-        </form>
-    </div>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function emailUpdateHandler(event) {
+        setEmail(event.target.value);
+    }
+
+    function passwordUpdateHandler(event) {
+        setPassword(event.target.value);
+    }
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="email">Your email</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={emailUpdateHandler}
+                />
+                <label htmlFor="password">Your password</label>
+                <input
+                    type="text"
+                    id="password"
+                    value={password}
+                    onChange={passwordUpdateHandler}
+                />
+            </form>
+        </div>
+    );
 }
