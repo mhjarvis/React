1b0c9e271108913d12a564a19d545e6d99580102
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-05 20:55:55 -0500

    Add Form component with basic setup for form

---
+++ Tutorials/00_React_Key_Concepts/src/components/React_06/Activity_6_1/Form.jsx
@@ -0,0 +1,10 @@
+export default function Form() {
+    return <div>
+        <form action="">
+            <label htmlFor="email">Your email</label>
+            <input type="email" id="email" />
+            <label htmlFor="password">Your password</label>
+            <input type="text" id='password' />
+        </form>
+    </div>
+}
