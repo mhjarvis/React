2f45383f8e5ddf6c70d53ab87c7f83b8c4c7eda8
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-05 20:55:35 -0500

    Start project folder

--- Tutorials/00_React_Key_Concepts/src/components/React_06/React_06.jsx
+++ Tutorials/00_React_Key_Concepts/src/components/React_06/React_06.jsx
@@ -2,12 +2,14 @@ import Title from "../Title";
 import "./React_06.css";
 import { questions_06 } from "./react_06_questions";
 import TestYourKnowledge from "../TestYourKnowledge";
+import Form from "./Activity_6_1/Form";
 
 export default function React_06() {
     return (
         <div className="sect">
             <Title>6. Styling React Apps</Title>
             <TestYourKnowledge questions={questions_06} />
+            <Form />
         </div>
     );
 }
