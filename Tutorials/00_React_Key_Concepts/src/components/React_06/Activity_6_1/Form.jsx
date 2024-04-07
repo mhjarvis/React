413eba13b62149e43fe30d1ed54daccd5607f0f5
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-07 17:07:34 -0500

    Add Activity 6.1 solution

--- Tutorials/00_React_Key_Concepts/src/components/React_06/Activity_6_1/Form.jsx
+++ Tutorials/00_React_Key_Concepts/src/components/React_06/Activity_6_1/Form.jsx
@@ -2,55 +2,71 @@ import { useState } from "react";
 import "./Form.css";
 
 export default function Form() {
+    let emailLabel,
+        emailInput,
+        passwordLabel,
+        passwordInput = "";
+
     const [email, setEmail] = useState("");
+    const [emailIsValid, setEmailIsValid] = useState(true);
     const [password, setPassword] = useState("");
+    const [passwordIsValid, setPasswordIsValid] = useState(true);
 
     function emailUpdateHandler(event) {
         setEmail(event.target.value);
-        testInputs();
     }
-    
 
     function passwordUpdateHandler(event) {
         setPassword(event.target.value);
-        testInputs();
     }
 
-    function testInputs() {
-        if (email.length < 8 || email.length > 0) {
-            console.log(email.length)
-            emailClass = "newRed";
+    function validateInput() {
+        if (!email.includes("@")) {
+            setEmailIsValid(false);
+        } else {
+            setEmailIsValid(true);
         }
-        if (email.length >= 8 || email.length === 0) {
-            emailClass = "";
+        if (password.length < 8) {
+            setPasswordIsValid(false);
+        } else {
+            setPasswordIsValid(true);
         }
     }
 
-    let emailClass;
-    let passwordClass;
+    if (emailIsValid === false) {
+        emailLabel = "red";
+        emailInput = "border-red";
+    }
+
+    if (passwordIsValid === false) {
+        passwordLabel = "red";
+        passwordInput = "border-red";
+    }
 
     return (
         <div>
             <form action="">
-                <label htmlFor="email" className="newRed">
+                <label htmlFor="email" className={emailLabel}>
                     Your email
                 </label>
                 <input
                     type="text"
-                    className={emailClass}
+                    className={emailInput}
                     id="email"
                     value={email}
                     onChange={emailUpdateHandler}
                 />
-                <label htmlFor="password">Your password</label>
+                <label htmlFor="password" className={passwordLabel}>
+                    Your password
+                </label>
                 <input
                     type="text"
-                    className={passwordClass}
+                    className={passwordInput}
                     id="password"
                     value={password}
                     onChange={passwordUpdateHandler}
                 />
-                <button onClick={testInputs}>Submit</button>
+                <button type="button" onClick={validateInput}>Submit</button>
             </form>
         </div>
     );
